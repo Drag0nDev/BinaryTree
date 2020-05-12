380e230c665d0bacf51adb2bf1e1daace88ac6ee
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-05-12 15:45:37 +0200

    finalized the basic wpf form to use with the tree

--- BinaryTree/MainWindow.xaml.cs
+++ BinaryTree/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Binary_Tree.BinaryTree;
 
 namespace BinaryTree
 {
@@ -7,9 +11,37 @@ namespace BinaryTree
     /// </summary>
     public partial class MainWindow
     {
+        private string s;
+        Tree myTree = new Tree();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            //input the new value in the unsorted list
+            if (unsortedList == null)
+            {
+                s = input.Text;
+            }
+            else
+            {
+                s += " " + input.Text;
+            }
+
+            unsortedList.Text = s;
+            
+            //input the new value in the Tree
+            myTree.AddRc(Int32.Parse(input.Text));
+            
+            //print sorted tree
+            string treeString = "";
+            myTree.Print(null, ref treeString);
+            sortedList.Text = treeString;
+            
+            //clear the input field
+            input.Clear();
         }
     }
 }
